e91368b6e629c440eaea5a02f8a7c9f5c5ed68dc
Alexkev <Alex.kmatheson@gmail.com>
2020-07-12 17:02:40 -0500

    UI update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   public numPlayer = 0;
-  public numPlayers: Array<number> = [];
+  public numPlayers: Array<number> = [0, 1];
 
   public addPlayer() {
     this.numPlayers.push(this.numPlayer);
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,14 +1,13 @@
-import { Component, Input } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Player } from '../player.model';
-import { PlayerService } from '../player.service';
+import { Component, Input } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Player } from "../player.model";
+import { PlayerService } from "../player.service";
 
 @Component({
-  selector: 'app-player',
-  templateUrl: './player.component.html',
-  styleUrls: ['./player.component.css']
+  selector: "app-player",
+  templateUrl: "./player.component.html",
+  styleUrls: ["./player.component.css"],
 })
-
 export class PlayerComponent {
   playerName: string;
   total: number;
@@ -16,7 +15,9 @@ export class PlayerComponent {
   points: number[] = [];
   arrayPoints: number[] = [0];
 
-  constructor(private playerService: PlayerService) {}
+  constructor(private playerService: PlayerService) {
+    this.addPlayer();
+  }
 
   getPoints(newPlayer: Player) {
     this.newPlayer = this.playerService.getPlayer(this.newPlayer);
@@ -28,10 +29,12 @@ export class PlayerComponent {
     this.total = this.arrayPoints.reduce((a, b) => a + b, 0);
   }
 
-  addPlayer(form: NgForm) {
-    const value = form.value;
-    this.playerName = value.name;
-    this.newPlayer = new Player(this.playerService.getId(), value.name , this.points);
+  addPlayer() {
+    this.newPlayer = new Player(
+      this.playerService.getId(),
+      "unknown",
+      this.points
+    );
     console.log(this.newPlayer);
     this.playerService.addPlayer(this.newPlayer);
   }
@@ -39,7 +42,11 @@ export class PlayerComponent {
   addPoints(form: NgForm) {
     const value = form.value;
     // const playerId = this.playerService.setId();
-    this.newPlayer = new Player(this.playerService.getId(), this.playerName, value.points);
+    this.newPlayer = new Player(
+      this.playerService.getId(),
+      this.playerName,
+      value.points
+    );
     console.log(this.newPlayer);
     this.playerService.addPoints(this.newPlayer);
     this.getPoints(this.newPlayer);
